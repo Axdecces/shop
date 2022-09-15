4fb7f2aa2b2383e4c8f5224b3fd9f31000870c96
Simon Wagner <axdecces@gmail.com>
2022-09-15 10:04:45 +0200

    Renamed django app

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShopApiConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop_api'
+    name = 'api'
