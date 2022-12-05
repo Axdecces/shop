2608f4c8b4686eff3451b0460cf403f7553ffde1
Simon Wagner <axdecces@gmail.com>
2022-12-05 15:16:26 +0100

    added token login

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets  import ModelViewSet
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 from api.models import Product, Tag, Customer, Cart, Supplier, Address, ShippingCompany, Order
 from api.serializers import (
@@ -25,6 +28,19 @@ class CustomerViewSet(ModelViewSet):
 	queryset = Customer.objects.all()
 	serializer_class = CustomerSerializer
 
+class CustomerLogIn(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token = Token.objects.get(user=user)
+        return Response({
+            'token': token.key,
+            'id': user.pk,
+            'username': user.username
+        })
 
 class CartViewSet(ModelViewSet):
 	queryset = Cart.objects.all()
